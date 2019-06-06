9479047fefd7c15bc0dc706ea4300bb408acd071
Michal Kasprzyk <emailer5k@gmail.com>
2019-06-06 19:41:38 +0200

    Fix redirect problems

--- script.js
+++ script.js
@@ -5,8 +5,20 @@ var url = document.getElementById("url-input").value;
 var output = document.getElementById("output");
 new ClipboardJS('.btn');
 
+var getHash = window.location.hash.substr(1)
 
-function request() {
+if (window.location.hash != "") {
+    $.getJSON(endpoint + "/" + getHash, function (data) {
+        data = data["result"];
+
+        if (data != null) {
+            window.location.href = data;
+        }
+
+    });
+};
+
+function requestUrl(url) {
     this.url = url;
     $.ajax({
         'url': endpoint + "/" + window.location.hash.substr(1),
@@ -15,7 +27,7 @@ function request() {
         'dataType': 'json',
         'contentType': 'application/json; charset=utf-8'
     })
-}
+};
 
 function getRandom() {
     var randomString = Math.random().toString(32).substring(2, 5) + Math.random().toString(32).substring(2, 5);
@@ -23,9 +35,9 @@ function getRandom() {
 };
 
 function getUrl() {
-    
-    var dataOk = url.startsWith("http://") || url.startsWith("https://") || url.startsWith("ftp://");
-    if (!dataOk) {
+    var url = document.getElementById("url-input").value;
+    var protocol_ok = url.startsWith("http://") || url.startsWith("https://") || url.startsWith("ftp://");
+    if (!protocol_ok) {
         newurl = "http://" + url;
         return newurl;
     } else {
@@ -33,7 +45,7 @@ function getUrl() {
     }
 };
 
-function hash() {
+function generateHash() {
     if (window.location.hash == "") {
         window.location.hash = getRandom();
     }
@@ -41,8 +53,8 @@ function hash() {
 
 shorten.addEventListener('click', function shorten() {
     var mainUrl = getUrl();
-    hash();
-    request(mainUrl);
+    generateHash();
+    requestUrl(mainUrl);
     output.innerHTML = window.location;
 });
 
